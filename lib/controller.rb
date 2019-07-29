c9ee1038660ef5b2e445cd0e7a9dd83191b2472f
DeerAntlers <kolerik@hotmail.fr>
2019-07-29 23:38:22 +0200

    c'est la berezina, mdr

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,3 +1,5 @@
+require 'gossip'
+
 class ApplicationController < Sinatra::Base
   get 'index.erb/views' do
   get 'new_gossip.erb/views/lib' do
@@ -5,6 +7,18 @@ class ApplicationController < Sinatra::Base
   end
  
 end
+
+class Gossip
+  @author
+  @content
 post '/gossips/new/' do
-  puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+  puts Gossip.new.save
+end
+
+post '/gossips/new/' do
+  puts "Salut, je suis dans le serveur"
+  puts "Ceci est le contenu du hash params : #{params}"
+  puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+  puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+  puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
 end
